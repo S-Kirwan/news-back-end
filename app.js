c7a7e2bf4178b4a64f0a30ac56481aefe8c8caa9
Sean Kirwan <seandarrakirwan@gmail.com>
2026-02-04 12:04:12 +0000

    Setup server structure with app.js and listen.js
    MVC architecture established. Router, Controller, Service and Model
    directories created

---
+++ app.js
@@ -0,0 +1,16 @@
+const	express = require("express");
+const	app = express();
+
+const	commentsRouter = require("./routes/comments.route.js");
+const	articlesRouter = require("./routes/articles.route.js");
+const	topicsRouter = require("./routes/topics.route.js");
+const	usersRouter = require("./routes/users.route.js");
+
+app.use(express.json());
+
+app.use("/api/articles", articlesRouter);
+app.use("/api/comments", commentsRouter);
+app.use("/api/topics", topicsRouter);
+app.use("/api/users", usersRouter);
+
+module.exports = app;
---
+++ listen.js
@@ -0,0 +1,6 @@
+const	app = require("./app.js");
+
+app.listen(9090, () =>
+{
+	console.log("Server listening on Port 9090 :)!");
+})
