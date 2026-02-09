9265108d793f44ea59b0ed14454faa9e85632208
Sean Kirwan <seandarrakirwan@gmail.com>
2026-02-09 10:38:42 +0000

    Article routing bugfix
    Fix routing for invalid methods.
    .all after endpoints for invalid methods changed to follow the same
    endpoint as the original route.
    
    Bug was having .all with just "/" after route

--- routes/articles.route.js
+++ routes/articles.route.js
@@ -2,11 +2,14 @@ const	express = require("express");
 const	router = express.Router();
 
 const	{ getAllArticles, getArticleById } = require("../controllers/articles.controller.js");
+const	getCommentsByArticle = require("../controllers/comments.controller.js");
 const	invalidMethod = require("../middleware/invalid-method-middleware.js");
 
 
 router.get("/", getAllArticles).all("/", invalidMethod);
 
-router.get("/:article_id", getArticleById).all("/", invalidMethod);
+router.get("/:article_id", getArticleById).all("/:article_id", invalidMethod);
+
+router.get("/:article_id/comments", getCommentsByArticle).all("/:article_id/comments", invalidMethod);
 
 module.exports = router;
